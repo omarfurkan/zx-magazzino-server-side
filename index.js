8f140705586f7e3eba2dddf14a9c64d8cefa9cb7
omarfurkan <omarfurkan92@gmail.com>
2022-09-03 17:18:56 +0600

    general setup with installation

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Ready to Fly')
+});
+
+
+app.listen(port, () => {
+    console.log('All done in port>>>', port)
+})
