501cfef819ca1b0fb9b226d9ee4445c3d6d33247
omarfurkan <omarfurkan92@gmail.com>
2022-09-03 19:47:59 +0600

    get all the document of inventory items

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 app.use(express.json());
 app.use(cors());
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.revch01.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db("warehouse").collection("inventoryItems");
+
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir())
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
