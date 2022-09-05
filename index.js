df7db917fe51e1730589e72317b641cba8974598
omarfurkan <omarfurkan92@gmail.com>
2022-09-05 11:12:55 +0600

    corection of npm and findOne impliment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -30,6 +30,13 @@ async function run() {
             const cursor = inventoryCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/itemDetail/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await inventoryCollection.findOne(query);
+            res.render(result);
         })
 
     }
