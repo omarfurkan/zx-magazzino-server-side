2273915d01d40d9cca84410cf250ba1b1c6dd584
omarfurkan <omarfurkan92@gmail.com>
2022-09-11 08:03:09 +0600

    all done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
@@ -26,6 +27,28 @@ async function run() {
         await client.connect();
         const inventoryCollection = client.db("warehouse").collection("inventoryItems");
 
+
+        // 
+        function verifyJWT(req, res, next) {
+            const authHeader = req.headers.authrization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                console.log('decoded>>>>>>>>>>', decoded);
+                req.decoded = decoded;
+                next();
+            })
+
+
+        }
+
+
+
         // get all items
         app.get('/items', async (req, res) => {
             const query = {};
@@ -44,12 +67,19 @@ async function run() {
         })
 
         // get items by query
-        app.get('/myitem/:email?', async (req, res) => {
+        app.get('/myitem/:email?', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email }
-            const cursor = inventoryCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (email === decodedEmail) {
+                const query = { email }
+                const cursor = inventoryCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
         })
 
 
@@ -61,6 +91,20 @@ async function run() {
             const result = await inventoryCollection.insertOne(doc);
             res.send(result);
         })
+
+
+        // Auth
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            console.log(user, 'from server')
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
+
+
 
         // delete an item
         app.delete('/item/:id', async (req, res) => {
